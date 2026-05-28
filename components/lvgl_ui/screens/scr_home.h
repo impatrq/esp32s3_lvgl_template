8cf3bdbd219efd7cd7ee073177da475e126f9d73
PulidoIgnacio <141677246+PulidoIgnacio@users.noreply.github.com>
2026-05-28 14:20:36 +0000

    Actividad 1

---
+++ components/lvgl_ui/screens/scr_home.h
@@ -0,0 +1,9 @@
+#ifndef __SCR_HOME_H__
+#define __SCR_HOME_H__
+
+void scr_home_prepare(void);
+void scr_home_init(void);
+void scr_home_uninit(void);
+void scr_home_step(void);
+
+#endif
---
+++ components/lvgl_ui/ui/ui.h
@@ -0,0 +1,12 @@
+#ifndef __UI_H__
+#define __UI_H__
+
+#include "lvgl.h"
+
+extern lv_obj_t *ui_scrSplash;
+extern lv_obj_t *ui_scrHome;
+extern lv_obj_t *ui_scrMain;
+
+void ui_init(void);
+
+#endif
